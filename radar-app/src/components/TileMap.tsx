816328faa622428be09b17b60b893827cffdc20f
Matt Kollada <mattkollada@gmail.com>
2024-06-24 13:59:26 -0400

    updated US and Global maps in TileMap

--- radar-app/src/components/TileMap.tsx
+++ radar-app/src/components/TileMap.tsx
@@ -3,6 +3,7 @@ import dynamic from 'next/dynamic';
 import 'leaflet/dist/leaflet.css';
 import styles from '../styles/PrecipitationMap.module.css'; // Importing the CSS module for styling
 
+// Dynamic import for MapContainer and TileLayer with SSR disabled
 const MapContainer = dynamic(
   () => import('react-leaflet').then((mod) => mod.MapContainer),
   { ssr: false }
@@ -16,9 +17,40 @@ interface TileMapProps {
   directory: string;
 }
 
-const TileMap: React.FC<TileMapProps> = ({ directory }) => {
+const legendColors = [
+  { value: 0.0, color: 'rgba(0, 0, 0, 0)' },
+  { value: 0.1, color: 'rgba(173, 216, 230, 1)' },
+  { value: 1.0, color: 'rgba(135, 206, 235, 1)' },
+  { value: 2.5, color: 'rgba(0, 255, 255, 1)' },
+  { value: 5.0, color: 'rgba(0, 255, 127, 1)' },
+  { value: 10.0, color: 'rgba(255, 255, 0, 1)' },
+  { value: 20.0, color: 'rgba(255, 165, 0, 1)' },
+  { value: 50.0, color: 'rgba(255, 69, 0, 1)' },
+  { value: 100.0, color: 'rgba(139, 0, 139, 1)' },
+];
+
+const ColorLegend: React.FC = () => {
+  const gradientColors = legendColors.map(color => color.color).join(', ');
+  return (
+    <div className={styles.legendContainer}>
+      <div className={styles.legendGradient} style={{ background: `linear-gradient(${gradientColors})` }}></div>
+      <div className={styles.legendLabels}>
+        {legendColors.map((item, index) => (
+          <div key={index} className={styles.legendLabel}>
+            {item.value}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const GlobalMap: React.FC<TileMapProps> = ({ directory }) => {
+  const mapRef = React.useRef(null);
+
   return (
     <div className={styles.mapContainer}>
+      <ColorLegend />
       <MapContainer
         center={[0, 0]}
         zoom={3}
@@ -26,6 +58,7 @@ const TileMap: React.FC<TileMapProps> = ({ directory }) => {
         worldCopyJump={true}
         maxBounds={[[85, -180], [-85, 180]]} // Set max bounds to prevent wrapping
         maxBoundsViscosity={1.0} // Makes panning to edges smoother
+        ref={mapRef}
       >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -34,11 +67,38 @@ const TileMap: React.FC<TileMapProps> = ({ directory }) => {
         <TileLayer
           url={`/tiles/${directory}/{z}/{x}/{y}.png`}
           attribution="&copy; Your Attribution Here"
-          opacity={0.5}
         />
       </MapContainer>
     </div>
   );
 };
 
-export default TileMap;
+export const USMap: React.FC<TileMapProps> = ({ directory }) => {
+  const mapRef = React.useRef(null);
+
+  return (
+    <div className={styles.mapContainer}>
+      <ColorLegend />
+      <MapContainer
+        center={[37.8, -96.9]} // Center over the US
+        zoom={4} // Appropriate zoom level for the US
+        className={styles.map}
+        worldCopyJump={true}
+        maxBounds={[[50, -125], [24, -66.9]]} // Set max bounds for the continental US
+        maxBoundsViscosity={1.0} // Makes panning to edges smoother
+        ref={mapRef}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; OpenStreetMap contributors"
+        />
+        <TileLayer
+          url={`/tiles/${directory}/{z}/{x}/{y}.png`}
+          attribution="&copy; MRMS Data Contributors"
+        />
+      </MapContainer>
+    </div>
+  );
+};
+
+export default { GlobalMap, USMap };
--- radar-app/src/pages/index.tsx
+++ radar-app/src/pages/index.tsx
@@ -1,76 +1,20 @@
 // pages/index.tsx
-import { useEffect, useState } from 'react';
-import PrecipitationMap from '@/components/PrecipitationMap';
-import MetarMap from '@/components/MetarMap';
-import axios from 'axios';
+import { useState } from 'react';
+
 import styles from '../styles/Home.module.css'; // Importing the CSS module for styling
 
 
-import { FeatureCollection, Geometry, GeoJsonProperties } from 'geojson';
-import TileMap from '@/components/TileMap';
-
-const useData = (dataType: 'precipitation' | 'metar' | '') => {
-  const [precipGeojsonData, setPrecipGeojsonData] = useState<FeatureCollection<Geometry, GeoJsonProperties>[]>([]);
-  const [metarGeojsonData, setMetarGeojsonData] = useState<FeatureCollection<Geometry, GeoJsonProperties>[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        if (dataType === 'precipitation') {
-          const response = await axios.get('/api/fetchPrecipitationData', {
-            params: {
-              count: 5,
-              lat: 90,
-              lon: 180,
-              startTime: '2024-06-17',
-              endTime: '2024-06-17',
-              measurement: 'precip_30mn',
-              file_type: 'geojson',
-            },
-          });
-          setPrecipGeojsonData(response.data.geojsonData);
-        } else if (dataType == 'metar'){
-          const response = await fetch('http://127.0.0.1:5000/metar');
-          console.log(response);
-          const data = await response.json();
-          setMetarGeojsonData([data]); // Ensure metarGeojsonData is always an array
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [dataType]);
-
-  return { precipGeojsonData, metarGeojsonData, loading };
-};
+import {USMap, GlobalMap} from '@/components/TileMap';
 
 const Home: React.FC = () => {
-  const [dataType, setDataType] = useState<'precipitation' | 'metar' | ''>('metar');
-  const [mapType, setMapType] = useState<'totalPrecipitation' | 'temperature' | 'humidity' | 'metar' | 'precipitation'>('totalPrecipitation');
-  const { precipGeojsonData, metarGeojsonData, loading } = useData(dataType);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const [mapType, setMapType] = useState<'GFS_precipitation' | 'MRMS_precipitation' >('GFS_precipitation');
 
   const renderMap = () => {
     switch (mapType) {
-      case 'totalPrecipitation':
-        return <TileMap directory='total_precipitation' />
-      case 'temperature':
-        return <TileMap directory='temperature' />
-      case 'humidity':
-        return <TileMap directory='humidity' />;
-      case 'metar':
-        return metarGeojsonData.length ? <MetarMap geojsonData={metarGeojsonData[0]} /> : <div>No METAR data available</div>;
-      case 'precipitation':
-        return precipGeojsonData.length ? <PrecipitationMap geojsonData={precipGeojsonData} metarData={metarGeojsonData} fileType="geojson" /> : <div>No precipitation data available</div>;
+      case 'GFS_precipitation':
+        return <GlobalMap directory='GFS_precipitation' />
+      case 'MRMS_precipitation':
+        return <USMap directory='MRMS_precipitation' />
       default:
         return null;
     }
@@ -80,25 +24,11 @@ const Home: React.FC = () => {
     <div className={styles.button_container}>
       <div>
         <button className={styles.button} onClick={() => {
-          setMapType('totalPrecipitation')
-          setDataType('')
-          }}>Total Precipitation Map</button>
+          setMapType('GFS_precipitation')
+          }}>Global Precipitation Radar Map</button>
         <button className={styles.button} onClick={() => {
-          setMapType('temperature')
-          setDataType('')
-          }}>Temperature Map</button>
-        <button className={styles.button} onClick={() => {
-          setMapType('humidity')
-          setDataType('')
-          }}>Humidity Map</button>
-        <button className={styles.button} onClick={() => {
-          setMapType('metar')
-          setDataType('metar')
-          }}>METAR Wind Data</button>
-        <button className={styles.button} onClick={() => {
-          setMapType('precipitation')
-          setDataType('precipitation')
-          }}>Precipitation Data</button>
+          setMapType('MRMS_precipitation')
+          }}>US Precipitation Radar Map</button>
       </div>
       {renderMap()}
     </div>
