fd9083d6644a04c94b54acaa0ef88aa48656e2c6
Matt Kollada <mattkollada@gmail.com>
2024-07-18 13:17:09 -0400

    fixing nexradmap bug

--- radar-app/src/components/NexradMap.tsx
+++ radar-app/src/components/NexradMap.tsx
@@ -1,45 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 import 'leaflet/dist/leaflet.css';
-import { useMapEvents } from 'react-leaflet';
 
-// Dynamically import components from react-leaflet
+// Dynamically import components from react-leaflet with ssr: false
 const MapContainer = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), { ssr: false });
 const TileLayer = dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), { ssr: false });
 const Marker = dynamic(() => import('react-leaflet').then(mod => mod.Marker), { ssr: false });
 const Popup = dynamic(() => import('react-leaflet').then(mod => mod.Popup), { ssr: false });
 
-const MapClickHandler = ({ onClick }: { onClick: () => void }) => {
-  useMapEvents({
-    click: onClick,
-  });
-  return null;
-};
-
 const NexradMap = () => {
   const [leaflet, setLeaflet] = useState<any>(null);
   const [stations, setStations] = useState<any[]>([]);
-  const [selectedStation, setSelectedStation] = useState<null|string>(null);
+  const [selectedStation, setSelectedStation] = useState<null | string>(null);
   const [overlayImage, setOverlayImage] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     // Ensure this code runs only on the client side
-    import('leaflet').then((L) => {
-      L.Icon.Default.mergeOptions({
-        iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
-        iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-        shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png'
+    if (typeof window !== 'undefined') {
+      import('leaflet').then((L) => {
+        L.Icon.Default.mergeOptions({
+          iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+          iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+          shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+        });
+
+        setLeaflet(L);
       });
 
-      setLeaflet(L);
-    });
-
-    // Fetch the JSON data
-    fetch('/nexrad_stations.json')
-      .then(response => response.json())
-      .then(data => setStations(data))
-      .catch(error => console.error('Error fetching the NEXRAD stations:', error));
+      // Fetch the JSON data
+      fetch('/nexrad_stations.json')
+        .then((response) => response.json())
+        .then((data) => setStations(data))
+        .catch((error) => console.error('Error fetching the NEXRAD stations:', error));
+    }
   }, []);
 
   const handleMarkerClick = (stationId: string) => {
@@ -47,8 +41,8 @@ const NexradMap = () => {
     setLoading(true);
     console.log(stationId);
     fetch(`/api/updateNexradData?site_code=${stationId}&variable_name=reflectivity`)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         console.log(data);
         const imageUrl = data.imageUrl; // Update this with the correct key from your response
         console.log(imageUrl);
@@ -56,7 +50,7 @@ const NexradMap = () => {
         setOverlayImage('/nexrad/' + imageUrl);
         setLoading(false);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error fetching the update data:', error);
         setLoading(false);
       });
@@ -73,12 +67,11 @@ const NexradMap = () => {
 
   return (
     <MapContainer center={[37.8, -96]} zoom={4} style={{ height: '100vh', width: '100%' }}>
-      <MapClickHandler onClick={handleMapClick} />
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      {stations.map(station => (
+      {stations.map((station) => (
         <Marker
           key={station.id}
           position={[station.latitude, station.longitude]}
@@ -90,7 +83,8 @@ const NexradMap = () => {
           }}
         >
           <Popup>
-            <strong>{station.name}</strong><br />
+            <strong>{station.name}</strong>
+            <br />
             {station.id}
           </Popup>
         </Marker>
@@ -143,8 +137,12 @@ const NexradMap = () => {
         }
 
         @keyframes spin {
-          0% { transform: rotate(0deg); }
-          100% { transform: rotate(360deg); }
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
         }
       `}</style>
     </MapContainer>
